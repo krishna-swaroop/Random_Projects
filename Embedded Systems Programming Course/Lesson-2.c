5c00bc413ad84f35a59760394ea9e8b5870374fc
Krishna Swaroop <38141608+krishna-swaroop@users.noreply.github.com>
2020-05-19 02:11:54 +0530

    Update Lesson-2.c

--- Embedded Systems Programming Course/Lesson-2.c
+++ Embedded Systems Programming Course/Lesson-2.c
@@ -1,1 +1,17 @@
 // Lesson-2
+// This lesson explores making changes to flow of control in a program.
+// It explores referencing the assembly code to track how the compiler handles loops when compared to explicit statements.
+
+int main() {
+  int counter =0;
+  while (counter < 21) {
+    ++counter;
+    if((counter & 1) != 0){
+      // do something when the counter is odd
+    }
+    else{
+      // do something when the counter is even
+    }
+  }
+  return 0;
+}
