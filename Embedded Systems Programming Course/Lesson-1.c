15688ec5ae4e7006645eda63f86e5d03c70a4318
Krishna Swaroop <38141608+krishna-swaroop@users.noreply.github.com>
2020-05-19 02:08:08 +0530

    Update Lesson-1.c

--- Embedded Systems Programming Course/Lesson-1.c
+++ Embedded Systems Programming Course/Lesson-1.c
@@ -1,3 +1,6 @@
+// Lesson-1
+// This exercise deals with exploring various views in the IAR workspace environment.
+// Setting up Debugger tools. Looking at assembly code. Tracking the value of counter etc.
 int main() {
   int counter = 0;
   ++counter;
