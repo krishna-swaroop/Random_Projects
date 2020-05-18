f5006e3bb2eb68ec7e3900db865dca8996c312b5
Krishna Swaroop <38141608+krishna-swaroop@users.noreply.github.com>
2020-05-19 02:06:17 +0530

    Create Lesson-1.c

---
+++ Embedded Systems Programming Course/Lesson-1.c
@@ -0,0 +1,26 @@
+int main() {
+  int counter = 0;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  
+  return 0;
+}  
